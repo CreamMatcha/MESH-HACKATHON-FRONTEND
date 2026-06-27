6b65d9edc4c2e9d0d370dbaa92c7cbab80742902
hello-ziwon <zwon0125@gmail.com>
2026-06-27 16:51:05 +0900

    feat: 바텀 네비게이션 수정

---
+++ app/build/generated/hilt/component_trees/debug/dagger/hilt/internal/processedrootsentinel/codegen/_com_mesh_voda_VodaApplication.java
@@ -0,0 +1,9 @@
+package dagger.hilt.internal.processedrootsentinel.codegen;
+
+import dagger.hilt.internal.processedrootsentinel.ProcessedRootSentinel;
+
+@ProcessedRootSentinel(
+    roots = "com.mesh.voda.VodaApplication"
+)
+public final class _com_mesh_voda_VodaApplication {
+}
---
+++ app/build/generated/ksp/debug/java/com/mesh/voda/VodaApplication_GeneratedInjector.java
@@ -0,0 +1,17 @@
+package com.mesh.voda;
+
+import dagger.hilt.InstallIn;
+import dagger.hilt.codegen.OriginatingElement;
+import dagger.hilt.components.SingletonComponent;
+import dagger.hilt.internal.GeneratedEntryPoint;
+import javax.annotation.processing.Generated;
+
+@OriginatingElement(
+    topLevelClass = VodaApplication.class
+)
+@GeneratedEntryPoint
+@InstallIn(SingletonComponent.class)
+@Generated("dagger.hilt.android.processor.internal.androidentrypoint.InjectorEntryPointGenerator")
+public interface VodaApplication_GeneratedInjector {
+  void injectVodaApplication(VodaApplication vodaApplication);
+}
