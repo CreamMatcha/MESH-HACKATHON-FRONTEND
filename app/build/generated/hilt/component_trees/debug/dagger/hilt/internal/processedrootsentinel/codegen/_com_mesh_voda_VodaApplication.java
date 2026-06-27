92debae15bb112f8f4ece49552dac5cbd5dc5849
hello-ziwon <zwon0125@gmail.com>
2026-06-27 17:08:28 +0900

    fix: 빌드 파일 git 추적 제거

--- app/build/generated/hilt/component_trees/debug/dagger/hilt/internal/processedrootsentinel/codegen/_com_mesh_voda_VodaApplication.java
+++
@@ -1,9 +0,0 @@
-package dagger.hilt.internal.processedrootsentinel.codegen;
-
-import dagger.hilt.internal.processedrootsentinel.ProcessedRootSentinel;
-
-@ProcessedRootSentinel(
-    roots = "com.mesh.voda.VodaApplication"
-)
-public final class _com_mesh_voda_VodaApplication {
-}
